d19f7a7b8d849db72bae5af39d314cff99b07f86
hecht-a <axel.hct@gmail.com>
2023-05-19 13:47:33 +0200

    feat: add Category entity

---
+++ src/Domain/Category/Category.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Domain\Category;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: false)]
+    #[Assert\NotBlank]
+    private string $name;
+
+    #[ORM\Column(type: Types::TEXT, nullable: false)]
+    #[Assert\NotBlank]
+    private string $description;
+
+    #[ORM\Column(type: Types::STRING, nullable: false)]
+    #[Assert\CssColor(formats: ['hex_long', 'hex_short'])]
+    private string $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
