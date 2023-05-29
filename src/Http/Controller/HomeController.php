1778e5944fd966d93cbb48d899b9e85adca98683
HECHT Axel <axel.hct@gmail.com>
2023-05-30 00:44:46 +0200

    feat: display recipes in template

--- src/Http/Controller/HomeController.php
+++ src/Http/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controller;
 
+use App\Domain\Recipe\Recipe;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,6 +11,10 @@ class HomeController extends AbstractController
     #[Route(path: '/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
+        $recipes = $this->em->getRepository(Recipe::class)->findAll();
+
+        return $this->render('pages/home.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
 }
