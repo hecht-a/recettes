28159b6ef339cf29abbfb7862c1adb74e150c913
HECHT Axel <axel.hct@gmail.com>
2023-05-31 00:49:51 +0200

    feat: style for recipe_show route's page

--- src/Http/Controller/RecipeController.php
+++ src/Http/Controller/RecipeController.php
@@ -12,6 +12,15 @@ class RecipeController extends AbstractController
     #[Route(path: '/{slug<[a-z0-9A-Z\-]+>}-{id<\d+>}', name: 'show')]
     public function show(Recipe $recipe, string $slug): Response
     {
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipes_show', [
+                'id' => $recipe->getId(),
+                'slug' => $recipe->getSlug()
+            ], 301);
+        }
 
+        return $this->render('recipes/show.html.twig', [
+            'recipe' => $recipe
+        ]);
     }
 }
---
+++ src/Http/Twig/TwigFormatTimeExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class TwigFormatTimeExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('format_time', $this->formatTime(...))
+        ];
+    }
+
+    public function formatTime(int $time)
+    {
+        $minutes = $time % 60;
+        $hour = ($time - $minutes) / 60;
+
+        if($hour > 0) {
+            return "{$hour}h{$minutes}";
+        }
+
+        return "{$minutes} minutes";
+    }
+}
