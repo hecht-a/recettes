8e133c364a5720172de2b3c434d3fd15c4b04dee
hecht-a <axel.hct@gmail.com>
2023-05-31 16:25:49 +0200

    feat: add registration page

---
+++ src/Http/Form/RegistrationFormType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Form;
+
+use App\Domain\Auth\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+        $builder
+            ->add('name', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                'attr' => [
+                    'placeholder' => "Nom d'utilisateur"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(min: 6, max: 255),
+                ],
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'minlength' => 6,
+                        'placeholder' => 'Mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'minlength' => 6,
+                        'placeholder' => 'Confirmer le mot de passe'
+                    ]
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
