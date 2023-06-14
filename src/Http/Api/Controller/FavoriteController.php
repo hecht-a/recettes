615341be95aba47f38614c2e02595d7210ce6e11
hecht-a <axel.hct@gmail.com>
2023-06-14 17:32:53 +0200

    feat: add favorite controller

---
+++ src/Http/Api/Controller/FavoriteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Api\Controller;
+
+use App\Domain\Recipe\Recipe;
+use App\Http\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class FavoriteController extends AbstractController
+{
+    #[Route(path: '/like/{id<\d+>}', name: 'like')]
+    #[IsGranted('ROLE_USER')]
+    public function like(Recipe $recipe): JsonResponse
+    {
+        $recipe->addUserFavorite($this->getUser());
+        $this->em->flush();
+
+        return new JsonResponse();
+    }
+
+    #[Route(path: '/dislike/{id<\d+>}', name: 'dislike')]
+    #[IsGranted('ROLE_USER')]
+    public function dislike(Recipe $recipe): JsonResponse
+    {
+        $recipe->removeUserFavorite($this->getUser());
+        $this->em->flush();
+
+        return new JsonResponse();
+    }
+}
